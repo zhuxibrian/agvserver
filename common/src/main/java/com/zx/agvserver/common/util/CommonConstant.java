235d22fb3f5b67de1b68b15f252ceb6e11e66e6d
zhuxibrian <zhuxibrian@icloud.com>
2017-07-13 14:47:14 +0800

    增加websocket接口
    HTML5规范

---
+++ common/src/main/java/com/zx/agvserver/common/util/CommonConstant.java
@@ -0,0 +1,8 @@
+package com.zx.agvserver.common.util;
+
+/**
+ * Created by zx on 2017/7/12.
+ */
+public class CommonConstant {
+    public static final Integer CURRENT_WANGING_NUMBER = 10;
+}
--- webapi/src/main/java/com/zx/agvserver/WebapiApplication.java
+++ webapi/src/main/java/com/zx/agvserver/WebapiApplication.java
@@ -1,12 +1,20 @@
 package com.zx.agvserver;
 
+import com.zx.agvserver.connector.controller.AgvController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EntityScan("com.zx.agvserver.domain")
 public class WebapiApplication {
+
+	@Bean
+	public AgvController webSocketController() {
+		System.out.println("调用webSocketController====》》》》");
+		return new AgvController();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebapiApplication.class, args);
